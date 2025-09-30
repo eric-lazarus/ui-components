1d6c9bec25355b35c36e2cf4a69f7a7f60ff4e44
Eric Bowman <57630594+eric-lazarus@users.noreply.github.com>
2025-09-30 17:53:49 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'react'
+import { Example } from '../lib/main'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Example />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+// import { dirname, resolve } from 'node:path'
+// import { fileURLToPath } from 'node:url'
+import react from '@vitejs/plugin-react'
+
+// const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    build: {
+      lib: {
+        entry: ['lib/main.js'],
+        formats: ['es']
+      }
+    }
+})
