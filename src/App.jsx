dd3f97207c3eeae27cd6699401379c348b61e2af
Eric Bowman <57630594+eric-lazarus@users.noreply.github.com>
2025-09-30 18:04:28 -0400

    Add entry points

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import 'react'
-import { Example } from '../lib/main'
-import './App.css'
+// import { Example } from '../lib/main'
+import * as component_lib from 'component-library'
 
 function App() {
-
+  console.log('component lib', component_lib)
   return (
     <>
-      <Example />
+      <div />
     </>
   )
 }
